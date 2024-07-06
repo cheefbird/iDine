953211233e1860e9e1d85b6801a143b7b5000720
cheefbird <temp@mail.com>
2024-07-05 21:00:34 -0700

    CheckoutView updates for tipping

--- iDine/CheckoutView.swift
+++ iDine/CheckoutView.swift
@@ -9,17 +9,40 @@ import SwiftUI
 
 struct CheckoutView: View {
     @EnvironmentObject var order: Order
+
     @State private var paymentType = "Cash"
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tipSize = 0
 
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    let tipTest = [10]
+    let tipSizes = [7, 10, 15, 0]
 
     var body: some View {
-        VStack {
+        Form {
             Section {
                 Picker("How do you want to pay?", selection: $paymentType) {
                     ForEach(paymentTypes, id: \.self) {
                         Text($0)
                     }
+                }
+                Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+                if addLoyaltyDetails {
+                    TextField("Enter your iDine ID", text: $loyaltyNumber)
+                }
+            }
+            Section("Add a tip?") {
+                Picker("Percentage:", selection: $tipSize) {
+                    ForEach(tipSizes, id: \.self) {
+                        Text("\($0)")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            Section("Total: $100") {
+                Button("Confirm Order") {
+                    // TODO: Place the order
                 }
             }
         }
